939fba791818e79fe562ab4893c144e387fa15e3
BrunoHunoff <brunohunoff@gmail.com>
2024-05-21 20:30:40 -0300

    listar e buscar titulo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import controllers.ObraDeArteController;
-import models.ObraDeArte;
-import models.Pintura;
 
 public class Main {
     public static void main(String[] args) {
         try{
-            ObraDeArteController.buscarObra(1);
+            ObraDeArteController.buscarObraId(1);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
 
     }
 }
--- src/controllers/ObraDeArteController.java
+++ src/controllers/ObraDeArteController.java
@@ -11,12 +11,28 @@ public abstract class ObraDeArteController {
         listaObras.add(obra);
     }
 
-    public static ObraDeArte buscarObra(int id) throws Exception{
+    public static ArrayList<ObraDeArte> listarObras() throws Exception{
+        if (listaObras.isEmpty()) {
+            throw new Exception("Não há obras cadastradas!");
+        }
+        return listaObras;
+    }
+
+    public static ObraDeArte buscarObraId(int id) throws Exception{
         for(ObraDeArte obra: listaObras) {
             if (obra.getId() == id) {
                 return obra;
             }
         }
         throw new Exception("Obra não encontrada");
+    }
+
+    public static ObraDeArte buscarObraTitulo(String titulo) throws Exception{
+        for (ObraDeArte obraDeArte: listarObras()) {
+            if (obraDeArte.getTitulo() == titulo) {
+                return obraDeArte;
+            }
+        }
+        throw new Exception("Obra não encontrada!");
     }
 }
