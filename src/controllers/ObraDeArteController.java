e7e3290fb2877d31bdfdd946f2537288e4f22f7d
BrunoHunoff <brunohunoff@gmail.com>
2024-05-21 20:47:01 -0300

    atualizar e remover obra

--- src/controllers/ObraDeArteController.java
+++ src/controllers/ObraDeArteController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import models.Escultura;
+import models.Fotografia;
 import models.ObraDeArte;
+import models.Pintura;
 
 import java.util.ArrayList;
 
@@ -34,5 +37,49 @@ public abstract class ObraDeArteController {
             }
         }
         throw new Exception("Obra não encontrada!");
+    }
+
+    public static void excluirObra(int id) throws Exception{
+        for (ObraDeArte obraDeArte : listaObras) {
+            if (obraDeArte.getId() == id) {
+                listaObras.remove(obraDeArte);
+                return;
+            }
+        }
+        throw new Exception("Obra não encontrada!");
+    }
+
+    public static void atualizarObra(Pintura pintura, int id, String titulo,
+                                     String artista, int anoCriacao, String localizacaoMuseu,
+                                     String tipoTinta, String movimentoArtistico) {
+        pintura.setId(id);
+        pintura.setTitulo(titulo);
+        pintura.setArtista(artista);
+        pintura.setAnoCriacao(anoCriacao);
+        pintura.setLocalizacaoMuseu(localizacaoMuseu);
+        pintura.setTipoTinta(tipoTinta);
+        pintura.setMovimentoArtistico(movimentoArtistico);
+    }
+
+    public static void atualizarObra(Escultura escultura, int id, String titulo,
+                                     String artista, int anoCriacao, String localizacaoMuseu,
+                                     String material) {
+        escultura.setId(id);
+        escultura.setTitulo(titulo);
+        escultura.setArtista(artista);
+        escultura.setAnoCriacao(anoCriacao);
+        escultura.setLocalizacaoMuseu(localizacaoMuseu);
+        escultura.setMaterial(material);
+    }
+
+    public static void atualizarObra(Fotografia fotografia, int id, String titulo,
+                                     String artista, int anoCriacao, String localizacaoMuseu,
+                                     String tecnica) {
+        fotografia.setId(id);
+        fotografia.setTitulo(titulo);
+        fotografia.setArtista(artista);
+        fotografia.setAnoCriacao(anoCriacao);
+        fotografia.setLocalizacaoMuseu(localizacaoMuseu);
+        fotografia.setTecnica(tecnica);
     }
 }
